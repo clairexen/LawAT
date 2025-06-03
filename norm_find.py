ec2cf3a4abb6df6d389b43761aca0ef1d90fcc88
Claire Xenia Wolf <claire@clairexen.net>
2025-06-03 05:29:22 +0200

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- norm_find.py
+++ norm_find.py
@@ -3,7 +3,8 @@
 # Shared freely under ISC license (https://en.wikipedia.org/wiki/ISC_license)
 
 from playwright.sync_api import sync_playwright
-import time, getopt, sys
+import time, getopt, sys, json
+from ptpython.repl import embed
 
 # Remember to launch
 #   mitmproxy --mode regular --listen-port 8080 -s mitmp_cache.py
@@ -12,16 +13,18 @@ useProxyMode = True
 
 useHeadlessMode = True
 printHttpRequests = False
+launchInteractiveRepl = False
 
 
-#%% Usage + Getopt
+#%% Usage + Getopt + Load Index
 
 def usage():
     print()
-    print(f"Usage: {sys.argv[0]} [options]")
+    print(f"Usage: {sys.argv[0]} [options] <norm-key>")
     print()
     print("    -P, --no-proxy ...... do not use local proxy on port 8080")
     print("    -H, --no-headless ... no headless mode (i.e. show browser window)")
+    print("    -i, --interactive ... launch interactive REPL before shutting down")
     print("    -r, --print-http .... print a log line for each HTTP request")
     print()
     print("Note: Run 'mitmproxy -s mitmp_cache.py' (or mitmweb) in another")
@@ -29,9 +32,9 @@ def usage():
     print()
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "ho:PHr",
+    opts, args = getopt.getopt(sys.argv[1:], "ho:PHir",
                                ["help", "output=", "no-proxy", "no-headless",
-                                "print-http"])
+                                "interactive", "print-http"])
 except getopt.GetoptError as err:
     # print help information and exit:
     print(err)  # will print something like "option -a not recognized"
@@ -48,10 +51,23 @@ for o, a in opts:
         useHeadlessMode = False
     elif o in ("-r", "--print-http"):
         printHttpRequests = True
+    elif o in ("-i", "--interactive"):
+        launchInteractiveRepl = True
     #elif o in ("-o", "--output"):
     #    output = a
     else:
         assert False, "unhandled option"
+
+if len(args) != 1:
+    usage()
+    sys.exit(2)
+
+with open("norm_index.json", "r") as f:
+    normindex = json.load(f)
+
+if (normkey := args[0]) not in normindex:
+    assert False, "unrecognized shortname"
+normdata = normindex[normkey]
 
 
 #%% Initialize Browser Context
@@ -82,11 +98,32 @@ if printHttpRequests:
 
 #%% Actual Playwright Script
 
-page.goto("https://example.com/")
-print(page.title())
+if normdata["type"] in ("BG", "BVG"):
+    # Fill out search form
+    page.goto("https://ris.bka.gv.at/Bundesrecht/")
+    page.locator("#MainContent_TitelField_Value").fill(normdata["title"])
+    page.locator("#MainContent_VonParagrafField_Value").fill("0")
+    page.locator("#MainContent_TypField_Value").fill(normdata["type"])
+    page.locator("#MainContent_SearchButton").click()
+
+    # Click through to complete norm
+    with context.expect_page() as new_page_info:
+        page.locator("a").get_by_text("heute", exact=True).click()
+    page = new_page_info.value
+    page.wait_for_load_state()
+
+    print(f"Document URL for {normkey}:", page.url)
+    normdata["docurl"] = page.url
 
 
-#%% Shutdown Playwright
+#%% Save Index + Shutdown Playwright
+
+with open("norm_index.json", "w") as f:
+    json.dump(normindex, f, indent=4)
+    print(file=f)
+
+if launchInteractiveRepl:
+    embed(globals(), locals())
 
 if not useHeadlessMode:
     time.sleep(3)
