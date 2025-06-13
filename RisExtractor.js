7c027e499a11700e487bf4505b4eac35e274204b
Claire Xenia Wolf <claire@clairexen.net>
2025-06-13 12:11:51 +0200

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- RisExtractor.js
+++ RisExtractor.js
@@ -1,6 +1,7 @@
 // RIS Extractor -- Copyright (C) 2025  Claire Xenia Wolf <claire@clairexen.net>
 // Shared freely under ISC license (https://en.wikipedia.org/wiki/ISC_license)
 
+// for debug annotations
 function getCssSelector(el) {
 	if (!(el instanceof Element)) return null;
 	const path = [];
@@ -27,37 +28,82 @@ function getCssSelector(el) {
 	return path.join(" > ");
 }
 
+risParList = []
+risContentBlocks = {}
+document.querySelectorAll("div.document > div.documentContent").forEach(el => {
+	parName = el.querySelector(":scope > h2.onlyScreenreader:first-child").
+			textContent.trimStart().trimEnd();
+	risContentBlocks[parName] = el;
+	risParList.push(parName);
+})
+
 class RisExAST {
-	constructor(baseElement, bodyElement) {
-		this.baseSelector = getCssSelector(baseElement);
-		this.bodySelector = getCssSelector(bodyElement);
+	constructor(parentObj, baseElement) {
+		this.parentObj = parentObj;
+		if (this.parentObj !== null)
+			this.parentObj.children.push(this);
+		this.baseElement = baseElement;
+		this.baseElementSelector = getCssSelector(baseElement);
 		this.properties = {};
+		this.preTokens = [];
 		this.children = [];
+		this.postTokens = [];
 	}
 
-	getBaseElement() { return document.querySelector(this.baseSelector); }
-	getBodyElement() { return document.querySelector(this.bodySelector); }
+	getBase() {
+		// return document.querySelector(this.baseElementSelector);
+		return this.baseElement;
+	}
 
 	set(key, value) {
 		this.properties[key] = value;
 	}
 
-	addChild(child) {
-		this.children.push(child);
-		return child;
+	get(key, defaultValue=null) {
+		if (key in this.properties)
+			return this.properties[key];
+		return defaultValue;
+	}
+
+	visitBlockListItem(pState, el) {
+		if (el.tagName == "H4") {
+			ast = new RisExAST(this, el)
+			if (el.classList.contains("UeberschrPara"))
+				return ast.parseParHeading(pState);
+			return ast.parseHeading(pState);
+		}
+
+		ast = new RisExAST(this, el);
+		ast.set("type", "unknown");
+		ast.set("text", el.textContent);
+		return ast;
+	}
+
+	parseHeading(pState) {
+		this.set("type", "heading");
+		this.set("text", el.textContent);
+	}
+
+	parseParHeading(pState) {
+		this.set("type", "par-heading");
+		this.set("text", el.textContent);
+	}
+
+	parseBlock(pState) {
+		this.set("type", "block");
+		el = this.getBase();
+		content = el.querySelector(":scope > div.embeddedContent > div > div.contentBlock");
+		content.querySelectorAll(":scope > *").forEach(item =>
+				this.visitBlockListItem(pState, item));
 	}
 }
 
-function runRisExtractor(blkIndex) {
-	blk = document.querySelector("div.document > div.documentContent:nth-of-type("+blkIndex+")");
-	blkId = blk.querySelector(":scope > div.embeddedContent").getAttribute("id");
-	blkBody = blk.querySelector(":scope > div.embeddedContent > div > div.contentBlock");
-	ast = new RisExAST(blk, blkBody);
-
-	items = blkBody.querySelectorAll(":scope > *")
-	for (let i = 0; i < items.length; i++) {
-		child = ast.addChild(new RisExAST(items[i], items[i]));
-	}
-
-	return ast;
+function risExtractor(parName) {
+	risExtractor.pState = { "parName": parName };
+	// risExtractor.pState.fooBar = 0;
+	el = risContentBlocks[parName];
+	risExtractor.ast = new RisExAST(null, el);
+	risExtractor.ast.set("parName", parName);
+	risExtractor.ast.parseBlock(risExtractor.pState, 1);
+	return risExtractor.ast;
 }
