7943706d0f8b8272cd6fa3a684b460863fdb5bcd
Claire Xenia Wolf <claire@clairexen.net>
2025-06-21 13:50:02 +0200

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- activate.sh
+++ activate.sh
@@ -6,8 +6,7 @@
 source .venv/bin/activate
 eval "$(declare -f deactivate | sed 's/^deactivate/_rex_venv_deactivate/')"
 rex() { ( cd "${VIRTUAL_ENV}/.." && .venv/bin/python3 -m RisExUtils "$@"; ); }
-risen() { ( cd "${VIRTUAL_ENV}/.." && RisEnQuery_Do_Print_Welcome_Message="1" \
-		.venv/bin/python RisEnQuery.py "${@:-shell}"; ); }
+risen() { ( cd "${VIRTUAL_ENV}/.." && .venv/bin/python3 RisEnQuery.py "${@:-shell}"; ); }
 deactivate() {
 	unset -f rex
 	unset -f risen
@@ -16,6 +15,6 @@ deactivate() {
 	unset -f deactivate
 }
 if [ -z "${VIRTUAL_ENV_DISABLE_PROMPT:-}" ] ; then
-	export PS1="(RisEn) ${PS1#${VIRTUAL_ENV_PROMPT}}"
-	export VIRTUAL_ENV_PROMPT='(RisEn) '
+	export PS1="(LawAT) ${PS1#${VIRTUAL_ENV_PROMPT}}"
+	export VIRTUAL_ENV_PROMPT='(LawAT) '
 fi
